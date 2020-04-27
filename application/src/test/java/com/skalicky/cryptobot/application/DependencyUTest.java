06a7500ef65f79aeaa08e871a27e495bb34d50e4
Tomas Skalicky <skalicky.tomas@gmail.com>
2020-04-27 16:26:57 +0200

    layer architecture and dependencies check via TNG ArchUnit - refactoring

--- application/src/test/java/com/skalicky/cryptobot/application/DependencyUTest.java
+++ application/src/test/java/com/skalicky/cryptobot/application/DependencyUTest.java
@@ -22,58 +22,15 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
+import static com.skalicky.cryptobot.application.TestConstants.BASE_PACKAGE;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAnyPackage;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideOutsideOfPackage;
 import static com.tngtech.archunit.lang.conditions.ArchConditions.onlyDependOnClassesThat;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
-@AnalyzeClasses(packages = LayeredArchitectureUTest.BASE_PACKAGE)
-public class LayeredArchitectureUTest {
-
-    static final String BASE_PACKAGE = "com.skalicky.cryptobot";
-    private static final String APPLICATION_LAYER_NAME = "Application";
-    private static final String BUSINESS_LOGIC_API_LAYER_NAME = "BusinessLogicApi";
-    private static final String BUSINESS_LOGIC_IMPL_LAYER_NAME = "BusinessLogicImpl";
-    private static final String EXCHANGE_CONNECTOR_FACADE_API_LAYER_NAME = "ExchangeConnectorFacadeApi";
-    private static final String EXCHANGE_CONNECTOR_FACADE_IMPL_LAYER_NAME = "ExchangeConnectorFacadeImpl";
-    private static final String EXCHANGE_CONNECTOR_API_LAYER_NAME = "ExchangeConnectorApi";
-    private static final String EXCHANGE_CONNECTOR_IMPL_LAYER_NAME = "ExchangeConnectorImpl";
-
-    @ArchTest
-    static final ArchRule test_layers_when_accessesRespectLayers_then_ok = layeredArchitecture()
-
-            .layer(APPLICATION_LAYER_NAME).definedBy(BASE_PACKAGE + ".application..")
-            .layer(BUSINESS_LOGIC_API_LAYER_NAME).definedBy(BASE_PACKAGE + ".businesslogic.api..")
-            .layer(BUSINESS_LOGIC_IMPL_LAYER_NAME).definedBy(BASE_PACKAGE + ".businesslogic.impl..")
-            .layer(EXCHANGE_CONNECTOR_FACADE_API_LAYER_NAME).definedBy(
-                    BASE_PACKAGE + ".exchange.*.connectorfacade.api..")
-            .layer(EXCHANGE_CONNECTOR_FACADE_IMPL_LAYER_NAME).definedBy(
-                    BASE_PACKAGE + ".exchange.*.connectorfacade.impl..")
-            .layer(EXCHANGE_CONNECTOR_API_LAYER_NAME).definedBy(
-                    BASE_PACKAGE + ".exchange.*.connector.api..")
-            .layer(EXCHANGE_CONNECTOR_IMPL_LAYER_NAME).definedBy(
-                    BASE_PACKAGE + ".exchange.*.connector.impl..")
-
-            .whereLayer(APPLICATION_LAYER_NAME).mayNotBeAccessedByAnyLayer()
-            .whereLayer(BUSINESS_LOGIC_API_LAYER_NAME).mayOnlyBeAccessedByLayers(
-                    APPLICATION_LAYER_NAME,
-                    BUSINESS_LOGIC_IMPL_LAYER_NAME)
-            .whereLayer(BUSINESS_LOGIC_IMPL_LAYER_NAME).mayOnlyBeAccessedByLayers(
-                    APPLICATION_LAYER_NAME)
-            .whereLayer(EXCHANGE_CONNECTOR_FACADE_API_LAYER_NAME).mayOnlyBeAccessedByLayers(
-                    APPLICATION_LAYER_NAME,
-                    BUSINESS_LOGIC_IMPL_LAYER_NAME,
-                    EXCHANGE_CONNECTOR_FACADE_IMPL_LAYER_NAME)
-            .whereLayer(EXCHANGE_CONNECTOR_FACADE_IMPL_LAYER_NAME).mayOnlyBeAccessedByLayers(
-                    APPLICATION_LAYER_NAME)
-            .whereLayer(EXCHANGE_CONNECTOR_API_LAYER_NAME).mayOnlyBeAccessedByLayers(
-                    APPLICATION_LAYER_NAME,
-                    EXCHANGE_CONNECTOR_FACADE_IMPL_LAYER_NAME,
-                    EXCHANGE_CONNECTOR_IMPL_LAYER_NAME)
-            .whereLayer(EXCHANGE_CONNECTOR_IMPL_LAYER_NAME).mayOnlyBeAccessedByLayers(
-                    APPLICATION_LAYER_NAME);
+@AnalyzeClasses(packages = BASE_PACKAGE)
+public class DependencyUTest {
 
     @ArchTest
     static final ArchRule test_dependencies_when_sharedPackage_then_dependsOnlyOn_shared_or_nonProject_packages =
